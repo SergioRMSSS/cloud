e65c5268924e13120302f4efd022282518696ecb
Sergio <srms1866@gmail.com>
2025-12-13 20:23:06 +0100

    pt-1-6

--- excercicis/pt-1-6/main.tf
+++ excercicis/pt-1-6/main.tf
@@ -97,25 +97,86 @@ resource "aws_security_group" "private_sg" {
     description = "SSH solo desde el Bastion"
     from_port = 22
     to_port = 22
-    protocol         = "tcp"
+    protocol = "tcp"
     security_groups  = [aws_security_group.bastion_sg.id]
   }
 
   ingress {
-    description     = "Comunicacion interna entre las subredes privadas"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
+    description = "Comunicacion interna entre las subredes privadas"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     security_groups = [aws_security_group.private_sg.id]
   }
 
   egress {
     description = "Permitimos todo el trafico saliente"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Aqui creo las calves 
+# Aqui creo las calves del bastion
+resource "tls_private_key" "bastion_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "bastion_kp" {
+  key_name = "bastion-key"
+  public_key = tls_private_key.bastion_key.public_key_openssh
+}
+
+# Aqui creo las claves de las instancias privadas
+resource "tls_private_key" "private_key" {
+  count = var.private_instance_count
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "private_kp" {
+  count = var.private_instance_count
+  key_name = "private-key"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+# Aqui creo los ficheros .pem
+resource "local_file" "bastion.pem" {
+  filename = "bastion.pem"
+  content = tls_private_key.bastion_key.private_key_pem
+  file_permission = "0400"
+}
+
+resource "local_file" "private.pem" {
+  count = var.private_instance_count
+  filename = "private-${count.index + 1}.pem"
+  content = tls_private_key.private_key[count.index].private_key_pem
+  file_permission = "0400"
+}
+
+# Aqui creo las instancias
+# Pero primero creo la IP elastica para el bastion
+resource "aws_eip" "bastion_eip" {
+  domain = "vpc"
+  instance = aws_instance.bastion.id
+}
+
+resource "aws_instance" "bastion" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public.id
+  security_groups = [aws_security_group.bastion_sg.id] 
+  key_name = aws_key_pair.bastion_kp.key_name
+}
+
+resource "aws_instance" "private" {
+  count = var.private_instance_count
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.private[count.index].id
+  security_groups = [ aws_security_group.private_sg.id ]
+  key_name = aws_key_pair.private_kp.key_name
+}
+
