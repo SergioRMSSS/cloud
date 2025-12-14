e03437984859e5a76a8d9b85cafaa5711771d5c4
Sergio <srms1866@gmail.com>
2025-12-14 18:32:35 +0100

    pt-1-6

--- excercicis/pt-1-6/main.tf
+++ excercicis/pt-1-6/main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "private_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    security_groups = [aws_security_group.private_sg.id]
+    self = true
   }
 
   egress {
